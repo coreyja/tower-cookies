363b779a45b8573695d32e196cd158b7131883da
Corey Alexander <coreyja@gmail.com>
2022-08-28 13:39:28 -0400

    Implement FromRequestParts instead of FromRequest

--- src/extract.rs
+++ src/extract.rs
@@ -1,18 +1,20 @@
 use crate::Cookies;
 use async_trait::async_trait;
-use axum_core::extract::FromRequest;
-use http::{Request, StatusCode};
+use axum_core::extract::FromRequestParts;
+use http::{request::Parts, StatusCode};
 
 #[async_trait]
-impl<State, Body> FromRequest<State, Body> for Cookies
+impl<State> FromRequestParts<State> for Cookies
 where
-    Body: Send + 'static,
     State: Send + Sync,
 {
     type Rejection = (http::StatusCode, &'static str);
 
-    async fn from_request(req: Request<Body>, _state: &State) -> Result<Self, Self::Rejection> {
-        req.extensions().get::<Cookies>().cloned().ok_or((
+    async fn from_request_parts(
+        parts: &mut Parts,
+        _state: &State,
+    ) -> Result<Self, Self::Rejection> {
+        parts.extensions.get::<Cookies>().cloned().ok_or((
             StatusCode::INTERNAL_SERVER_ERROR,
             "Can't extract cookies. Is `CookieManagerLayer` enabled?",
         ))
